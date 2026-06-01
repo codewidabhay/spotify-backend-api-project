815d6af311609e8379dab83c26b62a4d3d4ba012
codewidabhay <sirabhay0123@gmail.com>
2026-06-01 16:49:29 +0530

    Add home route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("Spotify Backend API is running successfully 🚀");
+});
+
 
 app.listen(3000, (req, res) =>{
     console.log('Server Started Successfully On Port 3000');
